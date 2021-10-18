e5add557456d6a98f5d6f0ea721c6c2cf3ac2693
keelanmc1 <91790708+keelanmc1@users.noreply.github.com>
2021-10-18 17:03:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO; 
+using System.Collections; 
+using System.Text; 
+
+namespace myapp
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+           
+            Console.WriteLine("Enter the string you would like to reverse >"); 
+            string str = Console.ReadLine(); 
+
+            Console.WriteLine("The reversed string is: "+reverseString(str)); 
+            
+        }
+
+        private static string reverseString (string str)
+        {
+            char[]charArray = str.ToCharArray(); 
+            Array.Reverse(charArray); 
+            return new string(charArray); 
+        }
+
+        
+            
+            
+            
+                
+
+        
+    }
+}
